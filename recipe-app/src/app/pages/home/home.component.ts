278c1e28998ab1a0fbcafc5ccc485ad2f938b79d
Teodor <teodor.binisor@gmail.com>
2025-04-15 12:44:18 +0300

    Second commit

--- recipe-app/src/app/pages/home/home.component.ts
+++ recipe-app/src/app/pages/home/home.component.ts
@@ -12,8 +12,21 @@ import { RecipesService } from '../../services/recipes.service';
 export class HomeComponent {
     recipes: Recipe[] = [];
 
+    dummyRecipes!: Recipe[];
+
+    errorMessage= '';
+    
+
     constructor(recipesService: RecipesService) {
         this.recipes = recipesService.recipes;
+        recipesService.getAllRecipes().subscribe({
+          next: (response) => {
+            this.dummyRecipes = response.recipes;
+          },
+          error: (err) => {
+            this.errorMessage = err;
+          }
+        })
     }
 
 }
