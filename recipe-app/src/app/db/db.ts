fa6da0216cf0c3a9a9d4098d1e4998962af6d162
Teodor <teodor.binisor@gmail.com>
2025-05-06 12:19:32 +0300

    hard coded api link

--- recipe-app/src/app/db/db.ts
+++ recipe-app/src/app/db/db.ts
@@ -1,10 +1,12 @@
 import { init, i, id, InstaQLEntity } from "@instantdb/core";
-import { environment } from "../../environments/environment";
+// import { environment } from "../../environments/environment";
 
 
 
 // ID for app: Lenovo-App
-const APP_ID = environment.APP_ID;
+// const APP_ID = environment.APP_ID;
+
+const APP_ID = "37dc3a49-3351-4bb7-abd3-63baf686d5e0";
 
 // Optional: Declare your schema!
 const schema = i.schema({
